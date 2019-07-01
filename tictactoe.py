5b16636b1f03566544782053795addde6091b6a8
Gian12315 <giancarlo.cytro@gmail.com>
2019-07-01 14:45:58 -0500

    Fixed checkIfWon function

--- tictactoe.py
+++ tictactoe.py
@@ -2,7 +2,7 @@ import random
 
 board = [[" ", " ", " "],
         [" ", " ", " "],
-        [" ", " ", " "]]
+        [" ", " ", " "]]   
 possibleMoves = [[7, 8, 9],
                 [4, 5, 6],
                 [1, 2, 3]]
@@ -36,20 +36,50 @@ def gameLoop():
     print("Tic Tac Toe Game")
     turn = random.choice(["X","O"])
     hasWon = False
+    position = None
     while hasWon is not True:
         printBoard(board)    
         print("\n\nIs {} turn.".format(turn))
-        position = int(input("Position: "))
-        if not (position >=1 or position <=9):
+        try:
+            position = int(input("Position: "))
+        except Exception as err:
+            print("Please, insert a valid number")
+            position = 0
+        if not (position >=1 and position <=9):
             print("Invalid move, please insert one of these values")
             printBoard(possibleMoves)
 
         isValid = makeMove(turn, position)
         if isValid is not True:
             continue
+        hasWon = checkIfWon(turn, board) 
+        if hasWon is True:
+            printBoard(board)
         if turn == "X":
             turn = "O"
         else:
             turn = "X"
+
+def checkIfWon(turn, board):
+    # This checks horizontal
+    for lists in board:
+        if lists.count(turn) == 3:
+            print ("{} has won!".format(turn))
+            return True
+    # This checks for vertical
+    for x in range(3):
+        ocurrences = 0
+        for y in range(3):
+            if board[y][x] == turn:
+                ocurrences = ocurrences + 1
+        if ocurrences == 3:
+            print ("{} has won!".format(turn))
+            return True
+    # This checks for diagonal
+    if (board[0][0] == turn and board[1][1] == turn and board[2][2] == turn) or \
+    (board[0][2] == turn and board[1][1] == turn and board[2][0] == turn):
+        print ("{} has won!".format(turn))
+        return True
+    return False
 
 gameLoop()
